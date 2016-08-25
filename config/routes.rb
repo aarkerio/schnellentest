03fd1e1bca467d013466c25ce29715d6c220e19f
aarkerio <mmontoya@gmail.com>
2016-08-24 19:33:43 -0500

    Update devise views

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :users
   resources :groups
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # You can have the root of your site routed with "root"
+  root 'groups#welcome'
 end
--- spec/factories/groups.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :group do
-    name "MyString"
-    description "MyString"
-  end
-end
