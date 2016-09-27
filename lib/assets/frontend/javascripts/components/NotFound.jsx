bcf6f3236f3e7424b9ab3802cb2131a1a95b355a
aarkerio <mmontoya@gmail.com>
2016-09-27 13:09:54 -0500

    Adding new Mocha test

--- lib/assets/frontend/javascripts/components/NotFound.jsx
+++ lib/assets/frontend/javascripts/components/NotFound.jsx
@@ -6,8 +6,8 @@ export default class NotFound extends Component {
   render() {
     return (
         <div className="container">
-          <h1>Error 404! Page not found.</h1>
-          <Link to="/tests/">Zuhause</Link>
+          <h1>Error 404! Page not found. Es tut uns leid.</h1>
+          <Link to="/">Zuhause</Link>
         </div>
     );
   }
