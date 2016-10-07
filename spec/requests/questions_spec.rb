64443d8f6453ace0ebfffadaebff7f78c85ab9a6
aarkerio <mmontoya@gmail.com>
2016-10-07 15:13:07 -0500

    Adding new request rspec

--- spec/requests/questions_spec.rb
+++ spec/requests/questions_spec.rb
@@ -1,10 +1,146 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe "Questions", type: :request do
-  describe "GET /questions" do
-    it "works! (now write some real specs)" do
-      get questions_path
-      expect(response).to have_http_status(200)
+RSpec.describe '/api/v1/questions', type: :request do
+  before(:each) do
+    Answer.delete_all
+    QuestionQuestion.delete_all
+    Question.delete_all
+  end
+
+  describe 'POST /listing/' do
+    it '.returns all the questions' do
+      user = FactoryGirl.create :user
+      FactoryGirl.create :question, title: 'Math question', user: user
+      FactoryGirl.create :question, title: 'Other Math question', user: user
+      post '/api/v1/questions/listing', params: { user_id: user.id, active: true },
+                                    headers: { 'Accept' => 'application/json' },  as: :json
+
+      json = JSON.parse(response.body)
+      expect(json.length).to eq(2)
+      expect(response.status).to eq 200
+      expect(json.second['title']).to match('Other Math question')
+    end
+  end
+
+  describe 'POST /get_one/' do
+    it '.returns one question' do
+      question = FactoryGirl.create :question, title: 'Summerian question'
+
+      post '/api/v1/questions/get_one', params: { id: question.id },
+                                    headers: { 'Accept' => 'application/json' }, as: :json
+      json = JSON.parse(response.body)
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq('application/json')
+      expect(json['title']).to match('Summerian question')
+    end
+  end
+
+  describe 'POST /create/' do
+    it '.creates a question' do
+      user = FactoryGirl.create :user
+      question = FactoryGirl.attributes_for :question
+      post '/api/v1/questions/create', params: question, headers: { 'Accept' => 'application/json' },  as: :json
+
+      json = JSON.parse(response.body)
+      expect(response.status).to eq 200
+      expect(json['error']).to match(false)
+    end
+    it '.not creates a questions' do
+      user = FactoryGirl.create :user
+      question = FactoryGirl.attributes_for :question, title: nil
+      post '/api/v1/questions/create', params: question,
+                                   headers: { 'Accept' => 'application/json' }, as: :json
+
+      json = JSON.parse(response.body)
+      expect(response.status).to eq 200
+      expect(json['error']).to match(true)
+    end
+  end
+
+  describe 'PATCH /update/' do
+    it '.updates a question' do
+      user = FactoryGirl.create :user
+      question = FactoryGirl.create :question, user: user
+      patch '/api/v1/questions/update', params: { id: question.id },
+                                    headers: { 'Accept' => 'application/json' }, as: :json
+
+      json = JSON.parse(response.body)
+      expect(response.status).to eq 200
+      expect(json['error']).to match(false)
+    end
+ end
+
+  describe 'POST /search/' do
+    it '.search for questions not already in the question' do
+      lexem      = 'Argentina'
+      user       = FactoryGirl.create :user
+      question       = FactoryGirl.create :question, user: user
+      question1  = FactoryGirl.create :question, question: "#{lexem} History"
+      FactoryGirl.create :question_question, question: question, question: question1
+      FactoryGirl.create :question, question: "#{lexem} Geography"
+      FactoryGirl.create :question, question: "Capitals from #{lexem}"
+
+      post '/api/v1/questions/search', params: { id: question.id, question: {id: question.id, terms: lexem} },
+                                   headers: { 'Accept' => 'application/json' }, as: :json
+
+      json = JSON.parse(response.body)
+      expect(response.status).to eq 200
+      expect(json.length).to eq 2
+      expect(json.second['question']).to match("Capitals from #{lexem}")
+    end
+  end
+
+  describe 'POST /linking/' do
+    it '.link questions with the current question' do
+      user       = FactoryGirl.create :user
+      question       = FactoryGirl.create :question, user: user
+      question_1  = FactoryGirl.create :question
+      question_quest = FactoryGirl.create :question_question, question: question, question: question_1
+      question_2  = FactoryGirl.create :question
+      question_3  = FactoryGirl.create :question
+
+      post '/api/v1/questions/linking', params: { id: question.id, question_ids: [question_2, question_3]},
+                                    headers: { 'Accept' => 'application/json' }, as: :json
+
+      json = JSON.parse(response.body)
+      # puts "response >>>  #{json}"
+      expect(response.status).to eq 200
+      expect(json['error']).to eq false
+    end
+  end
+
+  describe 'PATCH /reorder/' do
+    it '.link questions with the current question' do
+      user        = FactoryGirl.create :user
+      question        = FactoryGirl.create :question, user: user
+      question_1   = FactoryGirl.create :question
+      question_2   = FactoryGirl.create :question
+      question_3   = FactoryGirl.create :question
+      FactoryGirl.create :question_question, question: question, question: question_1, order: 1
+      FactoryGirl.create :question_question, question: question, question: question_2, order: 2
+      FactoryGirl.create :question_question, question: question, question: question_3, order: 3
+
+      patch '/api/v1/questions/reorder', params: { id: question.id, question:{question_id: question_1.id, id: question.id, way: 'down' }},
+                                     headers: { 'Accept' => 'application/json' }, as: :json
+
+      json = JSON.parse(response.body)
+      # puts "response >>>  #{json}"
+      expect(json['error']).to eq false
+    end
+  end
+
+  describe 'DELETE /delete/' do
+    it '.destroy one questions' do
+      user = FactoryGirl.create :user
+      question_1 = FactoryGirl.create :question, title: 'Math question', user: user
+      question_2 = FactoryGirl.create :question, title: 'Other Math question', user: user
+      delete "/api/v1/questions/delete/#{question_2.id}", params: {},
+                                                  headers: { 'Accept' => 'application/json' }, as: :json
+
+      json = JSON.parse(response.body)
+      # puts "response >>>  #{json} "
+      expect(response.status).to eq 200
+      expect(json['error']).to match(false)
     end
   end
 end
--- spec/requests/tests_spec.rb
+++ spec/requests/tests_spec.rb
@@ -1,108 +1,106 @@
 require 'spec_helper'
 
-RSpec.describe "/api/v1/tests", type: :request do
-
+RSpec.describe '/api/v1/tests', type: :request do
   before(:each) do
     Answer.delete_all
     TestQuestion.delete_all
     Question.delete_all
   end
 
-  describe "POST /listing/" do
-    it ".returns all the tests" do
-      user  = FactoryGirl.create :user
-      test1 = FactoryGirl.create :test, title: "Math test", user: user
-      test2 = FactoryGirl.create :test, title: "Other Math test", user: user
-      post "/api/v1/tests/listing", params: {user_id: user.id, active: true}, headers: { "Accept" => "application/json" },  as: :json
+  describe 'POST /listing/' do
+    it '.returns all the tests' do
+      user = FactoryGirl.create :user
+      FactoryGirl.create :test, title: 'Math test', user: user
+      FactoryGirl.create :test, title: 'Other Math test', user: user
+      post '/api/v1/tests/listing', params: { user_id: user.id, active: true },
+                                    headers: { 'Accept' => 'application/json' },  as: :json
 
       json = JSON.parse(response.body)
-      # puts "response >>>  #{json.second} "
       expect(json.length).to eq(2)
       expect(response.status).to eq 200
-      expect(json.second['title']).to match("Other Math test")
+      expect(json.second['title']).to match('Other Math test')
     end
   end
 
-  describe "POST /get_one/" do
-    it ".returns one test" do
-      test = FactoryGirl.create :test, title: "Summerian test"
+  describe 'POST /get_one/' do
+    it '.returns one test' do
+      test = FactoryGirl.create :test, title: 'Summerian test'
 
-      post "/api/v1/tests/get_one", params: {id: test.id}, headers: { "Accept" => "application/json" },  as: :json
-
-      #puts "response  #{response.body.inspect} "
+      post '/api/v1/tests/get_one', params: { id: test.id },
+                                    headers: { 'Accept' => 'application/json' }, as: :json
       json = JSON.parse(response.body)
       expect(response.status).to eq 200
-      expect(response.content_type).to eq("application/json")
-      expect(json['title']).to match("Summerian test")
+      expect(response.content_type).to eq('application/json')
+      expect(json['title']).to match('Summerian test')
     end
   end
 
-  describe "POST /create/" do
-    it ".creates a test" do
+  describe 'POST /create/' do
+    it '.creates a test' do
       user = FactoryGirl.create :user
       test = FactoryGirl.attributes_for :test
-      post "/api/v1/tests/create", params: test, headers: { "Accept" => "application/json" },  as: :json
+      post '/api/v1/tests/create', params: test, headers: { 'Accept' => 'application/json' },  as: :json
 
       json = JSON.parse(response.body)
-      #puts "response >>>  #{json} "
       expect(response.status).to eq 200
       expect(json['error']).to match(false)
     end
-    it ".not creates a tests" do
+    it '.not creates a tests' do
       user = FactoryGirl.create :user
       test = FactoryGirl.attributes_for :test, title: nil
-      post "/api/v1/tests/create", params: test, headers: { "Accept" => "application/json" },  as: :json
+      post '/api/v1/tests/create', params: test,
+                                   headers: { 'Accept' => 'application/json' }, as: :json
 
       json = JSON.parse(response.body)
-      #puts "response >>>  #{json} "
       expect(response.status).to eq 200
       expect(json['error']).to match(true)
     end
   end
 
-  describe "PATCH /update/" do
-    it ".updates a test" do
+  describe 'PATCH /update/' do
+    it '.updates a test' do
       user = FactoryGirl.create :user
-      test = FactoryGirl.attributes_for :test
-      patch '/api/v1/tests/update', params: test, headers: { "Accept" => "application/json" },  as: :json
+      test = FactoryGirl.create :test, user: user
+      patch '/api/v1/tests/update', params: { id: test.id },
+                                    headers: { 'Accept' => 'application/json' }, as: :json
 
       json = JSON.parse(response.body)
-      #puts "response >>>  #{json} "
       expect(response.status).to eq 200
       expect(json['error']).to match(false)
     end
  end
 
-  describe "POST /search/" do
-    it ".search for questions not already in the test" do
+  describe 'POST /search/' do
+    it '.search for questions not already in the test' do
       lexem      = 'Argentina'
       user       = FactoryGirl.create :user
       test       = FactoryGirl.create :test, user: user
       question1  = FactoryGirl.create :question, question: "#{lexem} History"
-      test_quest = FactoryGirl.create :test_question, test: test, question: question1
-      question2  = FactoryGirl.create :question, question: "#{lexem} Geography"
-      question3  = FactoryGirl.create :question, question: "Capitals from #{lexem}"
- 
-      post "/api/v1/tests/search", params: {id: test.id, test: {id: test.id, terms: lexem}}, headers: { "Accept" => "application/json" },  as: :json
+      FactoryGirl.create :test_question, test: test, question: question1
+      FactoryGirl.create :question, question: "#{lexem} Geography"
+      FactoryGirl.create :question, question: "Capitals from #{lexem}"
+
+      post '/api/v1/tests/search', params: { id: test.id, test: {id: test.id, terms: lexem} },
+                                   headers: { 'Accept' => 'application/json' }, as: :json
 
       json = JSON.parse(response.body)
-      # puts "response >>>  #{json}"
       expect(response.status).to eq 200
       expect(json.length).to eq 2
       expect(json.second['question']).to match("Capitals from #{lexem}")
     end
   end
 
-  describe "POST /linking/" do
-    it ".link questions with the current test" do
+  describe 'POST /linking/' do
+    it '.link questions with the current test' do
       user       = FactoryGirl.create :user
       test       = FactoryGirl.create :test, user: user
-      question1  = FactoryGirl.create :question
-      test_quest = FactoryGirl.create :test_question, test: test, question: question1
-      question2  = FactoryGirl.create :question
-      question3  = FactoryGirl.create :question
- 
-      post "/api/v1/tests/linking", params: {id: test.id, question_ids: [question2, question3]}, headers: { "Accept" => "application/json" },  as: :json
+      question_1  = FactoryGirl.create :question
+      test_quest = FactoryGirl.create :test_question, test: test, question: question_1
+      question_2  = FactoryGirl.create :question
+      question_3  = FactoryGirl.create :question
+
+      post '/api/v1/tests/linking', params: { id: test.id, question_ids: [question_2, question_3]},
+                                    headers: { 'Accept' => 'application/json' }, as: :json
 
       json = JSON.parse(response.body)
       # puts "response >>>  #{json}"
@@ -111,19 +109,19 @@ RSpec.describe "/api/v1/tests", type: :request do
     end
   end
 
-  describe "PATCH /reorder/" do
+  describe 'PATCH /reorder/' do
+    it '.link questions with the current test' do
+      user        = FactoryGirl.create :user
+      test        = FactoryGirl.create :test, user: user
+      question_1   = FactoryGirl.create :question
+      question_2   = FactoryGirl.create :question
+      question_3   = FactoryGirl.create :question
+      FactoryGirl.create :test_question, test: test, question: question_1, order: 1
+      FactoryGirl.create :test_question, test: test, question: question_2, order: 2
+      FactoryGirl.create :test_question, test: test, question: question_3, order: 3
 
-    it ".link questions with the current test" do
-         user        = FactoryGirl.create :user
-    test        = FactoryGirl.create :test, user: user
-    question1   = FactoryGirl.create :question
-    question2   = FactoryGirl.create :question
-    question3   = FactoryGirl.create :question
-    test_quest1 = FactoryGirl.create :test_question, test: test, question: question1, order: 1
-    test_quest2 = FactoryGirl.create :test_question, test: test, question: question2, order: 2
-    test_quest3 = FactoryGirl.create :test_question, test: test, question: question3, order: 3
-
-      patch "/api/v1/tests/reorder", params: {id: test.id, test:{question_id: question1.id, id: test.id, way: 'down'}}, headers: { "Accept" => "application/json" },  as: :json
+      patch '/api/v1/tests/reorder', params: { id: test.id, test:{question_id: question_1.id, id: test.id, way: 'down' }},
+                                     headers: { 'Accept' => 'application/json' }, as: :json
 
       json = JSON.parse(response.body)
       # puts "response >>>  #{json}"
@@ -131,12 +129,13 @@ RSpec.describe "/api/v1/tests", type: :request do
     end
   end
 
-  describe "DELETE /delete/" do
-    it ".destroy one tests" do
+  describe 'DELETE /delete/' do
+    it '.destroy one tests' do
       user = FactoryGirl.create :user
-      test1 = FactoryGirl.create :test, title: "Math test", user: user
-      test2 = FactoryGirl.create :test, title: "Other Math test", user: user
-      delete "/api/v1/tests/delete/#{test2.id}", params: {}, headers: { "Accept" => "application/json" },  as: :json
+      test_1 = FactoryGirl.create :test, title: 'Math test', user: user
+      test_2 = FactoryGirl.create :test, title: 'Other Math test', user: user
+      delete "/api/v1/tests/delete/#{test_2.id}", params: {},
+                                                  headers: { 'Accept' => 'application/json' }, as: :json
 
       json = JSON.parse(response.body)
       # puts "response >>>  #{json} "
@@ -144,6 +143,4 @@ RSpec.describe "/api/v1/tests", type: :request do
       expect(json['error']).to match(false)
     end
   end
-
 end
-
