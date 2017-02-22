30e79b2a9817877fdc822c1ce0d536dd06ae6960
aarkerio <mmontoya@gmail.com>
2017-02-21 19:07:46 -0600

    Update doc uploader

--- app/models/doc.rb
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-# frozen_string_literal: true
-class Doc < ApplicationRecord
-  belongs_to :user
-  mount_uploader :attachment, AttachmentUploader
-  validates :name, presence: true
-
-end
--- spec/factories/docs.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :doc do
-    name "MyString"
-    description "MyText"
-    file "MyString"
-    hash "MyString"
-    user nil
-  end
-end
