61738caef018bae871374b6f1588dbe0b7766edf
aarkerio <mmontoya@gmail.com>
2017-08-07 13:11:17 -0500

    sdasd

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170801215804) do
+ActiveRecord::Schema.define(version: 20170606194920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,8 @@ ActiveRecord::Schema.define(version: 20170801215804) do
     t.bigint "question_id", null: false
     t.bigint "test_id", null: false
     t.integer "order", null: false
+    t.index ["question_id", "test_id"], name: "index_question_tests_on_question_id_and_test_id"
+    t.index ["test_id"], name: "index_question_tests_on_test_id"
   end
 
   create_table "questions", id: :serial, force: :cascade do |t|
@@ -99,7 +101,6 @@ ActiveRecord::Schema.define(version: 20170801215804) do
     t.boolean "copyright"
     t.string "lang", default: "en", null: false
     t.integer "status", default: 0, null: false
-    t.integer "type"
     t.index ["searchtext"], name: "ques_idx", using: :gin
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
